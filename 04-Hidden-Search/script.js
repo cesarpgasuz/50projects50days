2313624972f66d304fded9a25da77743d8379a6f
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2023-02-27 16:10:14 -0600

    Update script.js

--- 04-Hidden-Search/script.js
+++ 04-Hidden-Search/script.js
@@ -1,6 +1,8 @@
 const searchButton = document.querySelector('.icon');
 const busquedaInput = document.querySelector('.busqueda');
+const input = document.querySelector('input');
 
 searchButton.addEventListener('click', () => {
     busquedaInput.classList.toggle('active');
+    input.focus();
 })

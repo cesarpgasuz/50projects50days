972099fbd11ce0c95306feb0c7922c1da788b2e8
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2023-04-02 10:11:42 -0600

    Update script.js

--- 17-Movie-App/script.js
+++ 17-Movie-App/script.js
@@ -3,7 +3,7 @@ const formulario = document.querySelector('#form');
 const termino = document.querySelector('#termino');
 const home = document.querySelector('#home');
 
-const apiKey = key_tmdb;
+const apiKey = `${key_tmdb}`;
 const urlDB = 'https://api.themoviedb.org/3/movie/popular';
 const url = `${urlDB}?api_key=${apiKey}&language=es-Es&page=1`;
 
